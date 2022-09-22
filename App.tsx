2c944c2c1dda416314b824e87ef87f36292c805f
MadinaSharapova97 <114133064+MadinaSharapova97@users.noreply.github.com>
2022-09-22 11:13:36 +0500

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+//
+import "bootstrap/dist/css/bootstrap.css";
+import 'react-toastify/dist/ReactToastify.css'
+
+//
+import { GlobalStyle } from "./assets/styles/GlobaStyle";
+
+import Router from "./route/Router";
+import Context from "./context/Context";
+
+export default function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Context>
+          <ToastContainer/>
+          <Router />
+        </Context>
+      </BrowserRouter>
+    </>
+  );
+}
